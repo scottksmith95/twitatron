d05394bf2a6f9858ae9c242a0edc10f4c9f9726f
Scott Smith <scottksmith95@gmail.com>
2014-10-13 07:56:04 -0700

    Starting point for article 2

---
+++ 02/server.js
@@ -0,0 +1,24 @@
+// Load required packages
+var express = require('express');
+var compression = require('compression');
+
+// Create our Express application
+var app = express();
+
+// Add content compression middleware
+app.use(compression());
+
+// Add static middleware
+var oneDay = 86400000;
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+router.get('/', function(req, res) {
+  res.end('Twitatron');
+});
+
+// Start the server
+app.listen(3000);
